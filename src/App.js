0763cec584d1ea2cffd826adf4ff69423f17ae0e
Gamal Ramadan <gamal.ramadan.fe@gmail.com>
2023-02-23 18:16:23 +0200

    NavBar Without Animation Scroll

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
 import './App.scss'
-// import $ from 'jquery';
+import NavBar from "./Components/NavBar/NavBar";
+import Home from '../src/Components/Views/Home'
+import Category from '../src/Components/Views/Category'
+import Listing from '../src/Components/Views/Listing'
+import ContactUs from '../src/Components/Views/Contact-Us'
 
+// import $ from 'jquery';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <NavBar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Category" element={<Category />} />
+          <Route path="/Listing" element={<Listing />} />
+          <Route path="/Contact-us" element={<ContactUs />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Container, Nav, Navbar, } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Logo from '../../Imgs/white-logo.png'
+import './NavBar.scss'
+const NavBar = () => {
+    return (
+        <>
+            <Navbar expand="md" className='py-3'>
+                <Container>
+                    <Navbar.Brand href="/Home">
+                        <img src={Logo} alt='Logo' />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link className='text-uppercase me-4 link text-md-start text-center' href="/Home">Home</Nav.Link>
+                            <Nav.Link className='text-uppercase me-4 link text-md-start text-center' href="/Category">Category</Nav.Link>
+                            <Nav.Link className='text-uppercase me-4 link text-md-start text-center' href="/Listing">Listing</Nav.Link>
+                            <Nav.Link className='text-uppercase me-4 link text-md-start text-center' href="/Contact-us">Contact us</Nav.Link>
+                        </Nav>
+                        <button className='btn text-capitalize d-lg-flex justify-content-center align-items-center ms-lg-5 ms-0 d-none px-3'>
+                            <i className="fa-solid fa-plus me-2"></i>
+                            add your listing
+                        </button>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <Outlet />
+        </>
+    )
+}
+
+export default NavBar
